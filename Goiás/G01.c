b7a56596e3bc8fe245f5d246734202a8c24064ce
ToffeeJubileu <jocileudojcc@gmail.com>
2025-01-22 21:29:01 +0000

    Goiás

--- Goiás/G01.c
+++ Goiás/G01.c
@@ -3,28 +3,27 @@
 int main() {
 
     int pontos;
-    float populacao, pib; 
-    char nome[20], area[20];
+    char nome[20], area[20], pib[20], populacao[20];
 
     printf("Digite o Nome de Uma Cidade que faz Parte do Estado de Goiás: \n");
     scanf("%s", &nome);
 
     printf("Digite o Número da População: \n");
-    scanf("%f", &populacao);
+    scanf("%s", &populacao);
 
     printf("Digite o Número da Área: \n");
     scanf("%s", &area);
 
     printf("Digite o PIB: \n");
-    scanf("%f", &pib);
+    scanf("%s", &pib);
 
     printf("Digite o Número de Pontos Turísticos: \n");
     scanf("%d", &pontos);
 
     printf("Cidade: %s\n", nome);
-    printf("População: %f\n", populacao);
-    printf("Área Km: %s\n", area);
-    printf("US$: %f\n", pib);
+    printf("População (Hab) : %s\n", populacao);
+    printf("Área (Km²): %s\n", area);
+    printf("US$ (Bi): %s\n", pib);
     printf("Pontos Turísticos: %d\n", pontos);
 
 return 0;
