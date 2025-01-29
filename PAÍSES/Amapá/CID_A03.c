8e1d84be7e14a0140343376c24862b9392a6cb81
ToffeeJubileu <jocileudojcc@gmail.com>
2025-01-29 20:22:55 +0000

    Aventureiro

--- PAÍSES/Amapá/CID_A03.c
+++ PAÍSES/Amapá/CID_A03.c
@@ -3,28 +3,35 @@
 int main() {
     
     int pontos;
-    char nome[20], area[20], pib[20], populacao[20];
+    float popul, por, area, pib, populacao;
+    char nome[20];
 
         printf("Digite o Nome de Uma Cidade que faz Parte do Estado de Amapá: \n");
         scanf("%s", &nome);
 
         printf("Digite o Número da População: \n");
-        scanf("%s", &populacao);
+        scanf("%f", &populacao);
 
         printf("Digite o Número da Área: \n");
-        scanf("%s", &area);
+        scanf("%f", &area);
 
         printf("Digite o PIB: \n");
-        scanf("%s", &pib);
+        scanf("%f", &pib);
 
         printf("Digite o Número de Pontos Turísticos: \n");
         scanf("%d", &pontos);
 
+        popul = (populacao / area);
+        por = (pib / populacao);
+
     printf("Cidade: %s\n", nome);
-    printf("População (Hab) : %s\n", populacao);
-    printf("Área (Km²): %s\n", area);
-    printf("US$ (Bi): %s\n", pib);
+    printf("População: %.2f HAB\n", populacao);
+    printf("Área: %.2f Km²\n", area);
+    printf("US$: %.2f Bi\n", pib);
     printf("Pontos Turísticos: %d\n", pontos);
+
+    printf("Pessoas por Km²: %.2f km²\n", popul);
+    printf("Salário por Pessoas: %.2f Min.\n", por);
 
 return 0;
 
